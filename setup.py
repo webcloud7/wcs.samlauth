f6c0232155a95e2189a142389eeddd2e8538445c
Mathias Leimgruber <m.leimgruber@webcloud7.ch>
2023-09-28 17:31:10 -0400

    Back to development: 1.0.0a4

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ tests_require = [
 
 setup(
     name="wcs.samlauth",
-    version="1.0.0a3",
+    version="1.0.0a4.dev0",
     description="SAML authentication for plone sites",
     long_description=long_description,
     long_description_content_type="text/markdown",
